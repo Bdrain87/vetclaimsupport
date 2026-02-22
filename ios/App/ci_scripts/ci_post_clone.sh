2dd6073dee7b21a05b5e205d3dded7dc7c333fa8
Blake Drain <blakedrain@Blakes-MacBook-Air.local>
2026-02-21 20:51:46 -0500

    Auto-increment build number from Xcode Cloud CI_BUILD_NUMBER

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-
 set -e
+
+echo "=== Auto-incrementing build number ==="
+if [ -n "$CI_BUILD_NUMBER" ]; then
+  echo "Setting build number to Xcode Cloud CI_BUILD_NUMBER: $CI_BUILD_NUMBER"
+  cd "$CI_PRIMARY_REPOSITORY_PATH/ios/App"
+  agvtool new-version -all "$CI_BUILD_NUMBER"
+  cd "$CI_PRIMARY_REPOSITORY_PATH"
+else
+  echo "Not running in Xcode Cloud, skipping build number increment"
+fi
 
 echo "=== Capacitor: Installing Node.js dependencies and building web app ==="
 
